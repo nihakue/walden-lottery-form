7ed2d6f6fb4df6091fb7136002550225173b65ba
nihakue <walden.gabrielw@gmail.com>
2019-12-07 22:30:01 +0000

    fix radio bug and change landing

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,13 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="Walden School Logo" />
         <h1>Walden School of Liberal Arts</h1>
-        <h2>Admission Lottery Application</h2>
-        {goAgain ? (
-          <p>Thank you for submitting an application to the lottery.</p>
-        ) : (
+        {goAgain ? <h2>Thank you for submitting an application to the lottery.</h2> : <h2>Admission Lottery Application</h2>}
+        {!goAgain && 
           <p>
             Please submit one application per student, per year you wish to be
             in the lottery.
           </p>
-        )}
+        }
         <a href={`#lottery-application`} onClick={(e: any) => {goAgain && window.location.reload()}}>
           {goAgain ? 'Submit another' : 'Begin'}
         </a>
--- src/Lottery.tsx
+++ src/Lottery.tsx
@@ -227,13 +227,13 @@ export function Lottery({onSubmitted}: {onSubmitted: () => void}) {
 
 function Term({title, desc, register, errors}: any) {
   return (
-    <Field name={`${title}.accept`} errors={errors}>
+    <Field name={title} errors={errors}>
       <label htmlFor={title}>{title}</label>
       <label htmlFor={title}>{desc}</label>
       <span className="radio-group">
-        <input ref={register({validate: (value: any) => value === 'accept'})} type="radio" name={`${title}.accept`} value="accept"></input>
+        <input ref={register({validate: (value: any) => value === 'accept'})} type="radio" name={title} value="accept"></input>
         <label htmlFor="accept">I accept</label>
-        <input ref={register} type="radio" name={`${title}.decline`} value="decline"></input>
+        <input ref={register} type="radio" name={title} value="decline"></input>
         <label htmlFor="decline">I do not accept</label>
       </span>
     </Field>
