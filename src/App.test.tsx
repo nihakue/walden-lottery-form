8b1c8c2c37070bdcf5f767c3b70bbf85d9883f58
nihakue <walden.gabrielw@gmail.com>
2019-12-07 23:55:48 +0000

    rename smoke test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders begin link (smoke test)', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Begin/i);
   expect(linkElement).toBeInTheDocument();
